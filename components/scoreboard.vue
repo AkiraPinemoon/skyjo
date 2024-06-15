4c25c15a900904904d8bac39f68beb87488d7eaf
Akira Pinemoon <paulik080705@gmail.com>
2024-06-15 14:55:59 +0200

    added highlight to current player in scoreboard

--- components/scoreboard.vue
+++ components/scoreboard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-w-56 flex flex-col gap-2 p-2 border border-slate-800 rounded-lg">
-        <div v-for="player of data" class="border border-slate-800 rounded-full h-16 w-full flex place-items-center gap-4 pr-5">
+        <div v-for="player of data" class="border border-slate-800 rounded-full h-16 w-full flex place-items-center gap-4 pr-5" :class="player.socketId == game?.data?.currentPlayerId ? ' border-lime-600' : ''">
             <img src="https://www.svgrepo.com/show/532363/user-alt-1.svg"
                 class="h-full border border-slate-800 bg-white rounded-full" />
             <span class="grow">{{ player.socketId == game?.owner.socketId ? "👑" : "" }}{{ player.username }}</span>
